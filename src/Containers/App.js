7a524e0451af5d5f1344ae4b07920be1e562521a
gauravkr2506 <gauravkr2506@gmail.com>
2020-05-16 19:49:17 +0530

    add hindi blog

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -2,19 +2,28 @@ import React from "react";
 import logo from "./../logo.svg";
 import "./App.css";
 import Add from "./../Components/Common/add";
+import Data from "./../Data/data";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div style={{ backgroundColor: "#ff6600", margin: 10 }}>
-          <h1 style={{ marginLeft: 10, color: "#472e00" }}>Blog</h1>
+          <h1 style={{ marginLeft: 10, color: "#472e00" }}>Hindi Vyang</h1>
         </div>
         <div style={{ margin: 10 }}>
           <div
-            style={{ width: "80%", float: "left", backgroundColor: "#851f00" }}
+            style={{
+              width: "78%",
+              float: "left",
+              color: "#33331f",
+              display: "block",
+              paddingRight: 10,
+              lineHeight: 1.9,
+              fontWeight: 10,
+            }}
           >
-            aaaaa
+            {Data[0].content}
           </div>
           <div
             style={{
